e45fe8d58a18577612141a3095207bd89c2b7dd4
DreaverZhao <1203302838@qq.com>
2024-12-28 16:24:04 +0800

    add launch file and parameters

--- scripts/sdf_randomforest.py
+++ scripts/sdf_randomforest.py
@@ -14,22 +14,20 @@ from sensor_msgs.msg import PointCloud2
 from visualization_msgs.msg import MarkerArray, Marker
 
 
-def create_point_cloud():
-    rng = np.random.RandomState(0)
-    random_tree_num = 10
-    points_per_tree = 50
+def create_point_cloud(num_trees, points_per_tree, tree_radius, min_x, max_x, min_y, max_y, min_z, max_z):
+    rng = np.random.RandomState() 
     tree_points = []
-    for i in range(random_tree_num):
+    for i in range(num_trees):
         # randomly generate root poses on xy plane
-        root_pose = rng.uniform([-10.0, -10.0, 0.0], [60.0, 10.0, 0.0], [1, 3])
+        root_pose = rng.uniform([min_x + 1, min_y + 1, 0.0], [max_x - 1, max_y -1, 0.0], [1, 3])
         # randomly generate tree height
-        tree_height = rng.uniform(1.0, 10.0)
+        tree_height = rng.uniform(5.0, max_z)
         # randomly generate tree radius
-        tree_radius = rng.uniform(0.1, 0.5)
+        tree_radius = rng.uniform(0.3, tree_radius)
         # generate tree points
         for j in range(points_per_tree):
             # randomly generate tree points
-            tree_point = rng.uniform([-tree_radius, -tree_radius, -10.0], [tree_radius, tree_radius, tree_height], [1, 3])
+            tree_point = rng.uniform([-tree_radius, -tree_radius, min_z], [tree_radius, tree_radius, tree_height], [1, 3])
             tree_points.append(root_pose + tree_point)
     return np.concatenate(tree_points, axis=0)
 
@@ -42,17 +40,6 @@ def point_cloud_to_voxel_grid(pc: np.ndarray, shape, res, origin_point):
     channels = indices[:, 2]
     vg[rows, cols, channels] = 1.0
     return vg
-
-
-def compute_param(res, points):
-    min_x = min(points[:, 0])
-    max_x = max(points[:, 0])
-    min_y = min(points[:, 1])
-    max_y = max(points[:, 1])
-    min_z = min(points[:, 2])
-    max_z = max(points[:, 2])
-    shape = [int((max_x - min_x) / res) + 1, int((max_y - min_y) / res) + 1, int((max_z - min_z) / res) + 1]
-    return shape, np.array([min_x, min_y, min_z], dtype=np.float32)
 
 
 def visualize_point_cloud(pub: rospy.Publisher, pc: np.ndarray):
@@ -126,24 +113,43 @@ def plot_arrows_rviz(pub, positions, directions):
 def main():
     rospy.init_node("random_forest_sdf")
 
-    pc_pub = rospy.Publisher("points", PointCloud2, queue_size=10)
-    sdf_pub = rospy.Publisher("sdf", PointCloud2, queue_size=10)
-    sdf_grad_pub = rospy.Publisher("sdf_grad", MarkerArray, queue_size=10)
+    # pc_pub = rospy.Publisher("points", PointCloud2, queue_size=10)
+    # sdf_pub = rospy.Publisher("sdf", PointCloud2, queue_size=10)
+    # sdf_grad_pub = rospy.Publisher("sdf_grad", MarkerArray, queue_size=10)
 
-    rospy.sleep(0.1)
+    # rospy.sleep(0.1)
 
-    pc = create_point_cloud()
+    # random forest parameters
+    min_x = rospy.get_param("~min_x", -10.0)
+    max_x = rospy.get_param("~max_x", 10.0)
+    min_y = rospy.get_param("~min_y", -10.0)
+    max_y = rospy.get_param("~max_y", 10.0)
+    min_z = rospy.get_param("~min_z", -10.0)
+    max_z = rospy.get_param("~max_z", 10.0)
+    num_trees = rospy.get_param("~num_trees", 10)
+    points_per_tree = rospy.get_param("~points_per_tree", 50)
+    tree_radius = rospy.get_param("~tree_radius", 0.5)
 
-    res = 0.2
-    # shape, origin_point = compute_param(res, pc)
-    origin_point = np.array([-10.0, -12.0, -10.0], dtype=np.float32)
-    shape = [350, 240, 110]
+    # sdf parameters
+    res = rospy.get_param("~resolution", 0.1)
+    min_x_sdf = rospy.get_param("~min_x_sdf", min_x)
+    max_x_sdf = rospy.get_param("~max_x_sdf", max_x)
+    min_y_sdf = rospy.get_param("~min_y_sdf", min_y)
+    max_y_sdf = rospy.get_param("~max_y_sdf", max_y)
+    min_z_sdf = rospy.get_param("~min_z_sdf", min_z)
+    max_z_sdf = rospy.get_param("~max_z_sdf", max_z)
+
+    pc = create_point_cloud(num_trees, points_per_tree, tree_radius, min_x, max_x, min_y, max_y, min_z, max_z)
+
+    origin_point = np.array([min_x_sdf, min_y_sdf, min_z_sdf], dtype=np.float32)
+    shape = np.array([int((max_x_sdf - min_x_sdf) / res) + 1, int((max_y_sdf - min_y_sdf) / res) + 1, int((max_z_sdf - min_z_sdf) / res) + 1], dtype=np.int32)
+
     vg = point_cloud_to_voxel_grid(pc, shape, res, origin_point)
     rospy.loginfo("Computing sdf...")
     sdf, sdf_grad = compute_sdf_and_gradient(vg, res, origin_point)
 
     rospy.loginfo("Saving sdf to file...")
-    prifix = "/home/dreaver/AcroflightDiffusionModel/save/"
+    prifix = rospy.get_param("~save_path", "")
     np.save(prifix + "sdf_param.npy", np.asarray([res, origin_point[0], origin_point[1], origin_point[2], shape[0], shape[1], shape[2]]))
     np.save(prifix + "sdf.npy", sdf)
     np.save(prifix + "sdf_grad.npy", sdf_grad)
